b3e20fae9dc015347a13a6000a334fb2bb059940
weilanwl <1273753708@qq.com>
2021-06-23 20:44:19 +0800

    up

--- pages/parameter/parameter.vue
+++ pages/parameter/parameter.vue
@@ -1,24 +1,39 @@
 <template>
 	<ui-sys :title="title">
-		<ui-card>
-			<view class="p-4 border-bottom flex-bar">
-				<view class="text-xl text-bold">styles</view>
-				<view class="ui-table-td">Object</view>
-				<view class="ui-table-td">{}</view>
+		<view class="ui-table table-border table-full ui-BG">
+			<view class="ui-table-header">
+				<view class="ui-table-tr ui-BG-1">
+					<view class="ui-table-th">属性名</view>
+					<view class="ui-table-th text-center">类型</view>
+					<view class="ui-table-th text-center">默认值</view>
+					<view class="ui-table-th">说明</view>
+				</view>
 			</view>
-			<view class="p-4">
-				<view class="p-2">参数说明:</view>
-				<view class="p-2">内部样式</view>
+			<view class="ui-table-body">
+				<view class="ui-table-tr" v-for="(item, index) in parameter" :key="index">
+					<view class="ui-table-td">{{ item.name }}</view>
+					<view class="ui-table-td text-center"><view class="ui-tag">{{ item.type }}</view></view>
+					<view class="ui-table-td text-center"><view class="ui-tag" v-if="item.default">{{ item.default }}</view></view>
+					<view class="ui-table-td">{{ item.info }}</view>
+				</view>
 			</view>
-		</ui-card>
-	</ui-sys>
+		</view>
+	</ui-sys> 
 </template>
 
 <script>
 export default {
 	data() {
 		return {
-			title: '参数'
+			title: '参数',
+			parameter: [
+				{ name: 'styles', type: 'Object', default: '{}', info: '内部样式' },
+				{ name: 'bg', type: 'String', default: 'ui-BG-1', info: '背景颜色' },
+				{ name: 'img', type: 'String', default: '/', info: '背景图' },
+				{ name: 'title', type: 'String', default: '/', info: '引入文字以开启默认导航' },
+				{ name: 'navBg', type: 'String', default: 'bg-blur', info: '默认导航的背景色' },
+				{ name: 'footer', type: 'Boolean', default: 'true', info: '页面底部展示信息' }
+			]
 		};
 	},
 	methods: {}
